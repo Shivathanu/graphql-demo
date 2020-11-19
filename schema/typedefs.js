719c2508a463fac7190b65390b8e0a7c89cdd971
Shivathanu <gcshivathanu@gmail.com>
2020-11-19 08:56:27 +0000

    feat: added example queries

--- schema/typedefs.js
+++ schema/typedefs.js
@@ -2,29 +2,41 @@ const { gql } = require("apollo-server");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-   type Book {
-    id: ID!
-    title: String!
-    author: String!
-    isbn: String!
-    url: String!
-  }
+	type BookCategory {
+		id: ID!
+		type: String
+	}
 
-  type Mutation {
-    createBook(title: String!, author: String!): Book
-  }
+	input BookCategoryInput {
+		id: ID!
+		type: String
+	}
 
-  type Query {
-    allBooks(filter: bookFilter): [Book!]!
-  }
-  
-  input bookFilter {
-    id: ID
-    title: String
-    author: String
-    isbn: String
-    url: String
-  }
+	type Book {
+		id: ID!
+		title: String!
+		author: String!
+		isbn: String!
+		url: String!
+		category: BookCategory
+	}
+
+	input bookFilter {
+		id: ID
+		title: String
+		author: String
+		isbn: String
+		url: String
+		category: BookCategoryInput
+	}
+
+	type Mutation {
+		createBook(title: String!, author: String!): Book
+	}
+
+	type Query {
+		allBooks(filter: bookFilter): [Book!]!
+	}
 `;
 
 module.exports = typeDefs;
